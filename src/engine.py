Fixed floating point arithmetic oversight in engine time calculation. added assignment epochs

src/engine.py
@@ -111,7 +111,11 @@ class Engine:
         current_state = copy.deepcopy(x0)
         running = True
         time = 0
-        while running:
+
+        for time in np.arange(0.0, self.maxtime, self.dt):
+
+            tick = time / self.dt
+
             # print("Time: {0:3.2E}".format(time))
             if self.collisions:
                 collisions = self.apriori_collisions(current_state, system.nagents, system.ntargets, time)
@@ -119,7 +123,7 @@ class Engine:
                 collisions = set()
 
             # thist, state_hist, assign_hist = system.update(time, current_state, self.dt)
-            thist, state_hist, assign_hist = system.update(time, current_state, collisions, self.dt)
+            thist, state_hist, assign_hist = system.update(time, current_state, collisions, self.dt, tick)
 
 
             newdf = pd.DataFrame(np.hstack((thist[:, np.newaxis],
@@ -128,7 +132,6 @@ class Engine:
 
             self.log(newdf)
 
-            time = time + self.dt
             if time > self.maxtime:
                 running = False
 

src/setup.py
@@ -256,7 +256,7 @@ def generate_initial_conditions(dim, agent_model, target_model, nagents, ntarget
             # Targets
             # r = 150 # circle radius
             # x02p = [circle(r, ntargets, t) for t in range(ntargets)]
-            x02p = np.random.uniform(-500, 500, (nagents,dim)) # position spread
+            x02p = np.random.uniform(-1000, 1000, (nagents,dim)) # position spread
             x02 = np.zeros((ntargets, dx))
             vel_range = 100
             for ii, tt in enumerate(x02):

src/systems.py
@@ -1,4 +1,5 @@
 
+from decimal import Decimal
 import scipy.integrate as scint
 import numpy as np
 
@@ -27,6 +28,7 @@ class OneVOne(System):
         self.ntargets = len(self.targets)
         self.apol = pol
 
+        self.assignment_epoch = 10 # every 10 ticks, perform assignment
         self.optimal_assignment = None
 
         self.costs = []
@@ -64,17 +66,20 @@ class OneVOne(System):
 
         return assignments, cost
 
-    def update(self, t0, x0, collisions, dt):
+    def update(self, t0, x0, collisions, dt, tick):
 
         # print("Warning: Assumes that Each Target is Assigned To")
         # print("Dont forget to fix this (easy fix)")
 
-        # TEST
-        # TODO need to avoid recomputing cost-to-go for dyn every time step, because optimal at t0
         if t0 == 0:
             assignment, cost = self.compute_assignments(t0, x0, collisions)
+            self.current_assignment = assignment
         if t0 > 0 and self.apol.__class__.__name__ != 'AssignmentDyn':
-            assignment, cost = self.compute_assignments(t0, x0, collisions)
+            if tick % self.assignment_epoch == 0:
+                print("------> ASSIGNMENT AT: ", t0)
+                assignment, cost = self.compute_assignments(t0, x0, collisions)
+            else:
+                assignment = self.current_assignment
         else:
             assignment = self.optimal_assignment
 